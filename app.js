c8d3a383cea13f990c554a6db83926b10bc38a7a
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-05-30 21:49:28 +0200

    another commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var qestion1 = prompt("put width of reactangle");
+var qestion2 = prompt("put height of reactagle");
+
+
+qestion1 = parseInt(qestion1);
+qestion2 = parseInt(qestion2);
+
+var answer = qestion1 * qestion2;
+
+document.write(" The area of Rectangle is " + answer + "cm");
+ 
