a89b2bf99ab48a177fb51b2c8c7ffc6eeccfc7f2
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-05-30 22:24:51 +0200

    adding circle

--- app.js
+++ app.js
@@ -11,13 +11,23 @@ document.write(" The area of Rectangle is " + answer + "cm");
 
 
 
-var qestion3 = prompt("add width of reactangle");
-var qestion4 = prompt("add height of reactagle");
-var qestion4 = prompt("add length of reactagle");
+var qestion3 = prompt("add width of secound reactangle");
+var qestion4 = prompt("add height of secound reactagle");
+var qestion5 = prompt("add length of secound reactagle");
 
 qestion3 = parseInt(qestion1);
 qestion4 = parseInt(qestion2);
 qestion5 = parseInt(qestion1);
 
 var answer_2 = qestion3 * qestion4 * qestion5;
-document.write("<br> The volum area of Rectangle is " + answer_2 + "cm3</br>");
+document.write("<br> The volum area of secound Rectangle is " + answer_2 + "cm3</br>");
+
+var qestion = prompt("add width of secound reactangle");
+
+
+
+
+var qestion6 = prompt("add an radius of the circle ");
+qestion6 = parseInt(qestion6);
+
+
